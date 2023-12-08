58bccf5f6c34801ca9f1b8ffaa482b3e50ca6842
Daniel Nyvoll <daniel@escali.no>
2023-12-08 15:42:54 +0100

    Report off 319 started

---
+++ code/fetchData.svelte
@@ -0,0 +1,18 @@
+<script>
+async function fetchData() {
+      try {
+        const response = await fetch(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${$selectedCoordinates.lat}&lon=${$selectedCoordinates.lng}`);
+
+        if (response.ok) {
+          const data = await response.json();
+          $temperature = data.properties.timeseries[0].data.instant.details.air_temperature;
+          $windSpeed = data.properties.timeseries[0].data.instant.details.wind_speed;
+          return data;
+        } else {
+          throw new Error(`Failed to fetch data. Status: ${response.status}`);
+        }
+      } catch (error) {
+        throw error;
+      }
+    }
+</script>
